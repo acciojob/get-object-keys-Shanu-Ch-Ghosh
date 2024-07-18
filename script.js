e9cd50f9f8e486c4cadbb74fb9f25c6d23287ba7
Shanu Chandra Ghosh <166943936+Shanu-Ch-Ghosh@users.noreply.github.com>
2024-07-18 21:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+// Create the student object
+var student = {
+  name: 'John Doe'
+};
+
+// Add getKeys method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Test the getKeys method
+console.log(student.getKeys()); // Output: ['name']
+
